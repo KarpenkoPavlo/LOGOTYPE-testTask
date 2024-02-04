bb03d5e4f298df92daa55dfc7508a55f3edf0d3b
Pavlo Karpenko <karpenkopasha19@outlook.com>
2024-02-04 15:46:05 +0200

    Adaptive - done

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const mainHeaderItem = document.querySelectorAll('.mainHeader-item');
 const mainSearchInput = document.querySelector('.mainSearch-input');
 const mainGame__container = document.querySelector('.mainGame__container');
 const footerLeft = document.querySelector('.footerLeft');
+const footerLeftTitle = document.querySelector('.footerLeft-title');
+const footerText = document.querySelector('.footerText');
 // Функція для зміни світлої-темної теми
 function toggleDayNight() {
     // Якщо кнопка bttDay має dsiplay: block то виконуємо код
@@ -22,6 +24,8 @@ function toggleDayNight() {
         mainSearchInput.style.color = "rgba(255, 255, 255, 1)";
         mainGame__container.style.backgroundColor = "rgba(14, 18, 25, 1)";
         footerLeft.style.backgroundColor = "rgba(32, 36, 52, 1)";
+        footerLeftTitle.style.color = "rgba(255, 255, 255, 1)";
+        footerText.style.color = "rgba(255, 255, 255, 1)";
         
         // За допомогою forEach перебираю та навішую на кожен елемент функцію в якій змінюю колір
         mainHeaderItem.forEach(function(item) {
@@ -39,6 +43,8 @@ function toggleDayNight() {
         mainSearchInput.style.color = "rgba(255, 255, 255, 1)";
         mainGame__container.style.backgroundColor = "rgba(255, 255, 255, 1)";
         footerLeft.style.backgroundColor = "rgba(255, 255, 255, 1)";
+        footerLeftTitle.style.color = "rgba(0, 0, 0, 1)";
+        footerText.style.color = "rgba(13, 208, 186, 1)";
 
         // За допомогою forEach перебираю та навішую на кожен елемент функцію в якій змінюю колір
         mainHeaderItem.forEach(function(item) {
@@ -47,3 +53,50 @@ function toggleDayNight() {
     }
 }
 /* night theme end */
+
+/* bar start */
+document.addEventListener('DOMContentLoaded', function() {
+    let ratingItems = document.querySelectorAll('.mainEvaluation-ratingItem');
+    
+    ratingItems.forEach(function(item, index) {
+        item.addEventListener('mouseover', function() {
+            for (let i = 0; i <= index; i++) {
+                ratingItems[i].classList.add('hovered');
+            }
+        });
+
+        item.addEventListener('mouseout', function() {
+            ratingItems.forEach(function(item) {
+                item.classList.remove('hovered');
+            });
+        });
+    });
+});
+/* bar end */
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var lightThemeToggle = document.getElementById('lightThemeToggle');
+//     var darkThemeToggle = document.getElementById('darkThemeToggle');
+//     var lightThemeLink = document.getElementById('lightTheme');
+//     var darkThemeLink = document.getElementById('darkTheme');
+
+//     lightThemeToggle.addEventListener('click', function() {
+//         lightThemeLink.removeAttribute('disabled');
+//         darkThemeLink.setAttribute('disabled', 'disabled');
+//     });
+
+//     darkThemeToggle.addEventListener('click', function() {
+//         darkThemeLink.removeAttribute('disabled');
+//         lightThemeLink.setAttribute('disabled', 'disabled');
+//     });
+// });
