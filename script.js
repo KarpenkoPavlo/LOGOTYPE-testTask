157b71f22190fed05df3d06285ee8e8376166b28
Pavlo Karpenko <karpenkopasha19@outlook.com>
2024-02-05 13:40:40 +0200

    Bar - done

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ let mainInfoItem = document.querySelectorAll('.mainInfo-item');
 let mainHeaderItem = document.querySelectorAll('.mainHeader-item');
 let mainHeaderImgLogo = document.querySelector('.mainHeaderImgLogo');
 
+let mainHideImg1 = document.querySelector('.mainHide-img1');
+let mainHideImg2 = document.querySelector('.mainHide-img2');
+let mainHideImg3 = document.querySelector('.mainHide-img3');
+let mainHideImg4 = document.querySelector('.mainHide-img4');
+
+let footerSocialImg1 = document.querySelector('.footerSocialImg1');
+let footerSocialImg2 = document.querySelector('.footerSocialImg2');
+let footerSocialImg3 = document.querySelector('.footerSocialImg3');
+let footerSocialImg4 = document.querySelector('.footerSocialImg4');
+let footerSocialImg5 = document.querySelector('.footerSocialImg5');
+let footerSocialImg6 = document.querySelector('.footerSocialImg6');
+
 
 function lightThemeToggle() {
     themeDayDark.href = "/dark-theme/dark.css";
@@ -17,6 +29,18 @@ function lightThemeToggle() {
     })
 
     mainHeaderImgLogo.src = "/img/mainHeader/mainHeaderLogoWhite.svg";
+
+    mainHideImg1.src = "/img/header-items/item1.svg";
+    mainHideImg2.src = "/img/header-items/item2.svg";
+    mainHideImg3.src = "/img/header-items/item3.svg";
+    mainHideImg4.src = "/img/header-items/item4.svg";
+
+    footerSocialImg1.src = "/img/footer/footer-socialWhite1.svg";
+    footerSocialImg2.src = "/img/footer/footer-socialWhite2.svg";
+    footerSocialImg3.src = "/img/footer/footer-socialWhite3.svg";
+    footerSocialImg4.src = "/img/footer/footer-socialWhite4.svg";
+    footerSocialImg5.src = "/img/footer/footer-socialWhite5.svg";
+    footerSocialImg6.src = "/img/footer/footer-socialWhite6.svg";
 };
 
 function darkThemeToggle() {
@@ -29,13 +53,27 @@ function darkThemeToggle() {
     })
 
     mainHeaderImgLogo.src = "/img/mainHeader/mainHeaderLogo.svg";
+
+    mainHideImg1.src = "/img/mainHide/mainHideItem1.svg";
+    mainHideImg2.src = "/img/mainHide/mainHideItem2.svg";
+    mainHideImg3.src = "/img/mainHide/mainHideItem3.svg";
+    mainHideImg4.src = "/img/mainHide/mainHideItem4.svg";
+
+    footerSocialImg1.src = "/img/footer/footer-social1.svg";
+    footerSocialImg2.src = "/img/footer/footer-social2.svg";
+    footerSocialImg3.src = "/img/footer/footer-social3.svg";
+    footerSocialImg4.src = "/img/footer/footer-social4.svg";
+    footerSocialImg5.src = "/img/footer/footer-social5.svg";
+    footerSocialImg6.src = "/img/footer/footer-social6.svg";
 };
 /* light-dark theme end */
 
 /* bar start */
 document.addEventListener('DOMContentLoaded', function() {
     let ratingItems = document.querySelectorAll('.mainEvaluation-ratingItem');
-    
+    let yourRatingElement = document.getElementById('yourRating');
+    let ratingTitle2 = document.querySelector('.ratingTitle2');
+
     ratingItems.forEach(function(item, index) {
         item.addEventListener('mouseover', function() {
             for (let i = 0; i <= index; i++) {
@@ -48,31 +86,71 @@ document.addEventListener('DOMContentLoaded', function() {
                 item.classList.remove('hovered');
             });
         });
+
+        item.addEventListener('click', function() {
+            let selectedRating = this.textContent;
+            yourRatingElement.textContent = selectedRating;
+
+            ratingItems.forEach(function(item, i) {
+                if (i <= index) {
+                    item.classList.add('selected');
+                } else {
+                    item.classList.remove('selected');
+                }
+            });
+
+            ratingTitle2.textContent = "13 votes";
+        });
     });
 });
 /* bar end */
 
 /*burger start*/
-function burgerF() {
-    let burger = document.querySelector(".header__burger");
-    let mainHeaderMenu = document.querySelector(".mainHeader-menu");
-    let mainSearchForm = document.querySelector('.mainSearch-form');
-  
-    burger.addEventListener("click", function() {
-      let computedStyle = window.getComputedStyle(mainHeaderMenu);
-  
-      // Додаю та видаляю на burger при кліці клас
-      this.classList.toggle("cross");
-  
-      if (computedStyle.display === "none") {
-        mainHeaderMenu.style.display = "block";
-        mainSearchForm.style.display = "block";
-      } else if (computedStyle.display === "block") {
-        mainHeaderMenu.style.display = "none";
-        mainSearchForm.style.display = "none";
-      }
-    });
-  }
-  
-  burgerF();
-  /*burger end*/
+function burger() {
+  let burger = document.querySelector(".header__burger");
+  let mainHeaderMenu = document.querySelector(".mainHeader-menu");
+  let mainSearchForm = document.querySelector('.mainSearch-form');
+  let mainHide = document.querySelector('.mainHide');
+
+  burger.addEventListener("click", function() {
+    let computedStyle = window.getComputedStyle(mainHeaderMenu);
+
+    // Додаю та видаляю на burger при кліці клас
+    this.classList.toggle("cross");
+
+    if (computedStyle.display === "none") {
+      mainHeaderMenu.style.display = "block";
+      mainSearchForm.style.display = "block";
+      mainHide.style.display = "block";
+    } else if (computedStyle.display === "block") {
+      mainHeaderMenu.style.display = "none";
+      mainSearchForm.style.display = "none";
+      mainHide.style.display = "none";
+    }
+  });
+}
+
+burger();
+/*burger end*/
+
+/* mainGame burger start */
+function mainGameBurger() {
+  let burger = document.querySelector(".mainGameBurger__burger");
+  let mainGameBurgerItems = document.querySelector('.mainGameBurger-items');
+
+  burger.addEventListener("click", function() {
+    let computedStyle = window.getComputedStyle(mainGameBurgerItems);
+
+    // Додаю та видаляю на burger при кліці клас
+    this.classList.toggle("cross");
+
+    if (computedStyle.display === "none") {
+        mainGameBurgerItems.style.display = "block";
+    } else if (computedStyle.display === "block") {
+        mainGameBurgerItems.style.display = "none";
+    }
+  });
+}
+
+mainGameBurger();
+/* mainGame burger end */
